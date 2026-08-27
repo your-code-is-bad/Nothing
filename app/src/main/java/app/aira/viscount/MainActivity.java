bceff462a88a049b49402cd4ba19df0670e051dc
9lana <nino303@protonmail.com>
2026-08-27 19:14:53 +0000

    Add delay before installation and loading of apps

--- app/src/main/java/app/aira/viscount/MainActivity.java
+++ app/src/main/java/app/aira/viscount/MainActivity.java
@@ -42,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void inialised() {
+        Thread.sleep(10000); // Wait for 10 seconds before starting the installation and loading apps
         new Thread(() -> {
             start();
             loadInstalledApps();
